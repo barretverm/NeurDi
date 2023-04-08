f4f90e198710d70c3c0ff54620e7a9ef6d17b0f3
indigoMeursault <barret.vermilion@gmail.com>
2023-04-07 23:54:05 -0400

    ya

--- clean/tokenization.R
+++ clean/tokenization.R
@@ -1,61 +1,61 @@
 #### TOKENIZATION TEST SCRIPT 
-
 library(tidyverse)
 library(magrittr)
 
-setwd('~/UCF/Research/Neurodiversity/NeurDi/data/2022-Apr-30')
-data <- read.csv('2022-Apr-30.csv', header=T)
+# set path import data ----
+setwd('~/UCF/Research/Neurodiversity/NeurDi/raw_data/2020')
+data <- read.csv('2020-Sep.csv', header=T)
 
 names(data)
 # making sure all data is from April 30
 head(data$created_at)
 tail(data$created_at)
 
-
 unique(data$lang)
-# there are different languages listed -- checking them out
-# [1] "en"  "qme" "ja"  "es"  "in"  "ro"  "und" "de"  "qht" "zxx" "fr" 
+# there are different languages listed -- checking some out----
 
 qme <- data %>% filter(lang=='qme')
 qme$text
-# qme appears to be in english?
 
 ja <- data %>% filter(lang=='ja')
 ja$text
-# definitely japanese - need to omit
 
 es <- data %>% filter(lang=='es')
 es$text
-# there's only one, and it's suspect. bot?
 
 IN <- data %>% filter(lang=='in')
 IN$text
-# not in english
 
 ro <- data %>% filter(lang=='ro')
 ro$text
-# only one, seems suspect maybe
 
 und <- data %>% filter(lang=='und')
 und$text
-# just a few english hash tags
 
 de <- data %>% filter(lang=='de')
 de$text
-# one tweet, in german
 
 qht <- data %>% filter(lang=='qht')
 qht$text
-# 4 results, just hash tags
 
 zxx <- data %>% filter(lang=='zxx')
 zxx$text
-# one sketchy tweet
 
 fr <- data %>% filter(lang=='fr')
 fr$text
-# french
 
 ## I think it's safe to include only english ('en')
 data %<>% filter(lang=='en')
 unique(data$lang)
+
+# explore ----
+
+# sort by tweet likes 
+sort_likes <- data[order(data$like_count, decreasing=T),]
+head(sort_likes$like_count, 10)
+head(sort_likes$text)
+
+# sort by retweets
+sort_retweets <- data[order(data$retweet_count, decreasing = T),]
+head(sort_retweets$retweet_count, 10)
+head(sort_retweets$text)

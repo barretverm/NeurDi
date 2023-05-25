7b6506210fa5b8986c3752da68fcab52cb288f0e
isaactuckey <isaactuckey@knights.ucf.edu>
2023-05-25 12:55:37 -0400

    updated README

---
+++ README.md
@@ -0,0 +1,28 @@
+# Neurodiversity Tweet Text Analysis
+
+The tweet data is has been retrieved/downloaded using R scripts. The data is stored in the folder [raw_data](raw_data) in .csv format organized by year and month.
+
+## Cleaning
+The data is cleaned and tokenized by this python script: [clean/tokenization.py](clean/tokenization.py). Cleaning currently maintains the following data for each tweet.
+- tweet_id
+- user_username (Twitter handle)
+- author_id
+- user_name *(Profile Name)*
+- text
+- created_at
+- in_reply_to_user_id
+- user_created_at
+- user_description
+- user_location
+- retweet_count
+- like_count
+- quote_count
+- user_followers_count
+- user_following_count
+- sourcetweet_type
+- **tokenized_text**
+
+Retweets are discarded. Cleaned data is saved to [clean](clean) organized by year and month in .csv format. The tokenization/cleaning script can be adjusted and run again to overwrite the cleaned data. Tokenized_text is a new column with tweets prepared for bag-of-word analysis as a list of tokens.
+
+## Analysis
+NLP models and other analysis methods are scripted in [analysis](analysis).
--- clean/tokenization.py
+++ clean/tokenization.py
@@ -1,7 +1,10 @@
-from nltk import TweetTokenizer
 from pathlib import Path
 import re
 import pandas as pd
+import nltk
+nltk.download('words')
+from nltk.corpus import words
+
 
 def clean(year, month):
     csv_path = 'raw_data/' + year + '/' + year + '-' + month + '.csv'
@@ -17,9 +20,8 @@ def clean(year, month):
     # keep english
     tweets_df = tweets_df[tweets_df['lang']=='en']
 
-    # remove retweets
+    # Remove retweets and unnecessary columns
     tweets_df = tweets_df[tweets_df['sourcetweet_type']!='retweeted']
-
     tweets_df = tweets_df[['tweet_id', 'user_username', 'author_id', 'user_name',
                         'text', 'created_at', 'in_reply_to_user_id',
                         'user_created_at', 'user_description', 'user_location',
@@ -27,21 +29,34 @@ def clean(year, month):
                         'user_followers_count','user_following_count', 'sourcetweet_type']]
 
     # Tokenizer
-    tk = TweetTokenizer(preserve_case=False, reduce_len=False, strip_handles=True)
-
+    tk = nltk.TweetTokenizer(preserve_case=False, reduce_len=False, strip_handles=True)
     tweets_df['tokenized_text'] = tweets_df['text'].apply(tk.tokenize)
 
     # Remove urls
     url_pattern = re.compile('https:\/\/t.co\/\w*')
     def remove_url(tokenized_tweet):
         return [word for word in tokenized_tweet if not url_pattern.match(word)]
-
     tweets_df['tokenized_text'] = tweets_df['tokenized_text'].apply(remove_url)
+
+    # Collect hashtags
+    hashtag_pattern = re.compile('#\S*')
+    def collect_hashtags(tokenized_tweet):
+        return [word for word in tokenized_tweet if hashtag_pattern.match(word)]
+    tweets_df['hashtags'] = tweets_df['tokenized_text'].apply(collect_hashtags)
+
+    # Collect foreign
+    foreign_pattern = re.compile('[^\w\d\s!\"#\$%&\'\(\)\*\+,-\.\/:;<=>\?@\[\\\]\^_`{\|}~]')
+    def collect_foreign(tokenized_tweet):
+        return [word for word in tokenized_tweet if foreign_pattern.match(word)]
+    tweets_df['foreign_characters'] = tweets_df['tokenized_text'].apply(collect_foreign)
 
     # Export cleaned df to csv
     filepath = Path('clean/'+ year + '/' + year + '-' + month + '.csv')
     filepath.parent.mkdir(parents=True, exist_ok=True)
     tweets_df.to_csv(filepath)
+
+    # Fix misspellings
+    correct_words = words.words()
 
 for year in ['2020', '2021', '2022', '2023']:
     for month in ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']:
